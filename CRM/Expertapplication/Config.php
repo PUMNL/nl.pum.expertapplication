60bf34708aad46b6872b128e512b17a699392cc5
Erik Hommel <hommel@ee-atwork.nl>
2014-09-11 13:32:47 +0200

    fix issue 302 api scheduled job to reactivate experts

--- CRM/Expertapplication/Config.php
+++ CRM/Expertapplication/Config.php
@@ -8,6 +8,17 @@ class CRM_Expertapplication_Config {
   
   private $drupal_role = false;
   
+  private $expertStatusColumn = null;
+  private $expertStatusCustomFieldId = null;
+  private $expertStatusEndDateColumn = null;
+  private $expertStatusStartDateColumn = null;
+  private $expertStatusReasonColumn = null;
+  private $expertStatusEndDateCustomFieldId = null;
+  private $expertDataCustomTable = null;
+  private $expertTempInactOption = null;
+  private $expertActiveOption = null;
+  private $expertContactType = null;
+  
   private function __construct() {
     $this->activity_types = unserialize(CRM_Core_BAO_Setting::getItem('nl.pum.expertapplication', 'new_expert_role_activities'));
     if (!is_array($this->activity_types)) {
@@ -19,6 +30,11 @@ class CRM_Expertapplication_Config {
     if (isset($roles[$rid])) {
       $this->drupal_role = $rid;
     }
+    /*
+     * config options required for api Expert Reactivate
+     */
+    $this->setExpertCustomData();
+    $this->setExpertContactType();
   }
   
   public static function singleton() {
@@ -39,10 +55,111 @@ class CRM_Expertapplication_Config {
   public function getDrupalRoles() {
     $roles = array();
     if (function_exists('user_roles')) {
-      $roles = user_roles(true);
+      $roles = user_roles(true); 
     }
     return $roles;
   }
   
+  public function getExpertContactType() {
+    return $this->expertContactType;
+  }
+  
+  public function getExpertDataCustomTable() {
+    return $this->expertDataCustomTable;
+  }
+  
+  public function getExpertStatusColumn() {
+    return $this->expertStatusColumn;
+  }
+  
+  public function getExpertStatusEndDateColumn() {
+    return $this->expertStatusEndDateColumn;
+  }
+  
+  public function getExpertStatusStartDateColumn() {
+    return $this->expertStatusStartDateColumn;
+  }
+  
+  public function getExpertStatusReasonColumn() {
+    return $this->expertStatusReasonColumn;
+  }
+  
+  public function getExpertStatusCustomFieldId() {
+    return $this->expertStatusCustomFieldId;
+  }
+  
+  public function getExpertStatusEndDateCustomFieldId() {
+    return $this->expertStatusEndDateCustomFieldId;
+  }
+  
+  public function getExpertTempInactOption() {
+    return $this->expertTempInactOption;
+  }
+  
+  public function getExpertActiveOption() {
+    return $this->expertActiveOption;
+  }
+  
+  private function setExpertContactType() {
+    $this->expertContactType = 'Expert';
+  }
+  
+  private function setExpertCustomData() {
+    try {
+      $customGroup = civicrm_api3('CustomGroup', 'Getsingle', array('name' => 'expert_data'));
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception("Could not find a custom group with name 'expert_data', "
+        . "error from API CustomGroup Getsingle : ".$ex->getMessage());
+    }
+    $this->expertDataCustomTable = $customGroup['table_name'];
+    $this->setExpertStatusCustomFields($customGroup['id']);
+  }
+  
+  private function setExpertStatusCustomFields($customGroupId) {
+    try {
+      $customFields = civicrm_api3('CustomField', 'Get', array('custom_group_id' => $customGroupId));
+    } catch (CiviCRM_API3_Exception $ex) {
+      throw new Exception("Could not find custom fields in custom group 'expert_data', "
+        . "error from API CustomField Get : ".$ex->getMessage());
+    }
+    foreach ($customFields['values'] as $customField) {
+      switch ($customField['name']) {
+        case 'expert_status':
+          $this->expertStatusColumn = $customField['column_name'];
+          $this->expertStatusCustomFieldId = $customField['id'];
+          $this->setExpertStatusOptions($customField['option_group_id']);
+          break;
+        case 'expert_status_end_date':
+          $this->expertStatusEndDateCustomFieldId = $customField['id'];
+          $this->expertStatusEndDateColumn = $customField['column_name'];
+          break;
+        case 'expert_status_start_date':
+          $this->expertStatusStartDateColumn = $customField['column_name'];
+          break;
+        case 'expert_status_reason':
+          $this->expertStatusReasonColumn = $customField['column_name'];
+          break;
+      }
+    }
+  }
+  
+  private function setExpertStatusOptions($optionGroupId) {
+    try {
+      $optionValues = civicrm_api3('OptionValue', 'Get', array('option_group_id' => $optionGroupId));
+    } catch (CiviCRM_API3_Exception $ex) {
+      $this->expertTempInactOption = null;
+      throw new Exception("Could not find option values in option group ".$optionGroupId.
+        ", error from API OptionValue Get : ".$ex->getMessage());
+    }
+    foreach($optionValues['values'] as $optionValue) {
+      switch ($optionValue['name']) {
+        case 'Active':
+          $this->expertActiveOption = $optionValue['value'];
+          break;
+        case 'Temporarily_inactive':
+          $this->expertTempInactOption = $optionValue['value'];
+          break;
+      }
+    }
+  }
 }
-
---
+++ api/v3/Expert/Reactivate.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Expert.Reactivate API
+ * 
+ * Reads all contacts with contact_sub_type Expert where the expert status
+ * is temporarily inactive and end date expert status is not empty
+ * For every found: if end date is today or before, set status back to
+ * active.
+ *
+ * @param array $params (empty, required for api workflow)
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws Exception when civicrm_api throws one
+ */
+function civicrm_api3_expert_reactivate($params) {
+  $expertApplicationConfig = CRM_Expertapplication_Config::singleton();
+  $expertStatusField = 'custom_'.$expertApplicationConfig->getExpertStatusCustomFieldId();
+  $expertStatusEndDateField = 'custom_'.$expertApplicationConfig->getExpertStatusEndDateCustomFieldId();
+  $params = array(
+    'contact_sub_type' => $expertApplicationConfig->getExpertContactType(),
+    'is_deleted' => 0,
+    $expertStatusField => $expertApplicationConfig->getExpertTempInactOption(),
+    'return' => $expertStatusEndDateField
+  );
+  try {
+    $contacts = civicrm_api3('Contact', 'Get', $params);
+  } catch (CiviCRM_API3_Exception $ex) {
+    throw new Exception('API error when retrieving contacts with API Contact Get : '.$ex->getMessage());
+  }
+  foreach ($contacts['values'] as $tempInactiveExpert) {
+    $endDate = date('Ymd', strtotime($tempInactiveExpert[$expertStatusEndDateField]));
+    if ($endDate <= date('Ymd')) {
+      _reactivateExpert($tempInactiveExpert['contact_id']);
+    }
+  }  
+  return civicrm_api3_create_success(array(), array(), 'Expert', 'Reactivate');
+}
+
+function _reactivateExpert($entityId) {
+  $expertApplicationConfig = CRM_Expertapplication_Config::singleton();
+  $reActQry = 'UPDATE '.$expertApplicationConfig->getExpertDataCustomTable().
+    ' SET '.$expertApplicationConfig->getExpertStatusColumn().
+    ' = %1, '.$expertApplicationConfig->getExpertStatusEndDateColumn().' = %2, '
+    .$expertApplicationConfig->getExpertStatusStartDateColumn().' = %3, '
+    .$expertApplicationConfig->getExpertStatusReasonColumn().' = %4 WHERE entity_id = %5';
+  $reActParams = array(
+    1 => array($expertApplicationConfig->getExpertActiveOption(), 'String'),
+    2 => array(null, 'Date'),
+    3 => array(date('Ymd'), 'Date'),
+    4 => array('Reactivated by automatic job', 'String'),
+    5 => array($entityId, 'Positive')
+  );
+  CRM_Core_DAO::executeQuery($reActQry, $reActParams);
+}
+
