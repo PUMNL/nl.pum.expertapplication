c4c0a5f1aaca4dfd6795bd3e8dc8f5ce6d1de866
Jaap Jansma <jaap@edeveloper.nl>
2016-10-20 09:47:39 +0200

    Update changed activity type Fill out pum CV label

--- CRM/Expertapplication/Upgrader.php
+++ CRM/Expertapplication/Upgrader.php
@@ -9,5 +9,16 @@ class CRM_Expertapplication_Upgrader extends CRM_Expertapplication_Upgrader_Base
     $this->executeCustomDataFile('xml/expert_data_agree_privacy_scheme.xml');
     return TRUE;
   }
+
+  public function upgrade_1003() {
+    $activity_type_group_id = civicrm_api3('OptionGroup', 'getvalue', array(
+      'name' => 'activity_type',
+      'return' => 'id',
+    ));
+    CRM_Core_DAO::executeQuery("UPDATE civicrm_option_value SET label = 'Filled Out PUM CV' WHERE name = 'Fill Out PUM CV' AND option_group_id = %1", array(
+      1 => array($activity_type_group_id, 'Integer')
+    ));
+    return TRUE;
+  }
   
 }
